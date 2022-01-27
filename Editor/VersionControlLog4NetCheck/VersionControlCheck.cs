b5b694214198fab5c4c7dbdb521868b2bb348e18
Rodrigo Varga <varguiniano@gmail.com>
2022-01-27 16:50:06 +0100

    - Improved package checking.

--- Editor/VersionControlLog4NetCheck/VersionControlCheck.cs
+++ Editor/VersionControlLog4NetCheck/VersionControlCheck.cs
@@ -1,3 +1,4 @@
+using System.IO;
 using UnityEditor;
 using UnityEditor.PackageManager;
 using UnityEditor.PackageManager.Requests;
@@ -18,16 +19,51 @@ namespace WhateverDevs.Core.Editor.VersionControlLog4NetCheck
         private const string PackageName = "com.unity.collab-proxy";
 
         /// <summary>
+        /// Recommended package version.
+        /// </summary>
+        private const string RecommendedVersion = "1.15.9";
+
+        /// <summary>
+        /// Path to data.
+        /// </summary>
+        private const string DataFolder = "Assets/Data/";
+
+        /// <summary>
+        /// Path to VersionControl data.
+        /// </summary>
+        private const string VersionControlFolder = DataFolder + "VersionControl/";
+
+        /// <summary>
+        /// Path to editor VersionControl data.
+        /// </summary>
+        private const string VersionControlEditorFolder = VersionControlFolder + "Editor/";
+
+        /// <summary>
+        /// Path to settings file.
+        /// </summary>
+        private const string SettingsPath = VersionControlEditorFolder + "VersionControlSettings.asset";
+
+        /// <summary>
+        /// Reference to the settings.
+        /// </summary>
+        private static VersionControlPackageCheckConfig settings;
+
+        /// <summary>
         /// Object to store listing packages request.
         /// </summary>
-        private static readonly ListRequest listRequest;
+        private static readonly ListRequest ListRequest;
 
         /// <summary>
         /// Check if the version control package is installed, the version and suggest to reboot.
         /// </summary>
         static VersionControlCheck()
         {
-            listRequest = Client.List();
+            CheckSettings();
+
+            // ReSharper disable once PossibleNullReferenceException
+            if (settings.CheckDismissed) return;
+            
+            ListRequest = Client.List();
             EditorApplication.update += OnVersionCheckFinished;
         }
 
@@ -36,28 +72,59 @@ namespace WhateverDevs.Core.Editor.VersionControlLog4NetCheck
         /// </summary>
         private static void OnVersionCheckFinished()
         {
-            if (!listRequest.IsCompleted) return;
+            if (!ListRequest.IsCompleted) return;
 
             EditorApplication.update -= OnVersionCheckFinished;
 
-            if (listRequest.Status == StatusCode.Failure)
+            if (ListRequest.Status == StatusCode.Failure)
             {
                 Debug.LogError("Error listing packages.");
                 return;
             }
 
-            foreach (PackageInfo packageInfo in listRequest.Result)
+            foreach (PackageInfo packageInfo in ListRequest.Result)
             {
                 if (packageInfo.name != PackageName) continue;
 
-                if (packageInfo.version == packageInfo.versions.latestCompatible) continue;
+                if (packageInfo.version.Equals(RecommendedVersion)) continue;
 
                 if (EditorUtility.DisplayDialog("Whatever Dev's Core",
-                                                "The core needs to update the Version Control package to compile. If you still get assembly errors after updating it, restart Unity.",
-                                                "Update it",
+                                                "The core needs to update the Version Control package to version 1.15.9 compile. If you still get assembly errors after updating it, restart Unity.",
+                                                "Update",
                                                 "I'll do it myself"))
-                    Client.Add(packageInfo.name + "@" + packageInfo.versions.latestCompatible);
+                    Client.Add(packageInfo.name + "@" + RecommendedVersion);
+                else
+                {
+                    settings.CheckDismissed = true;
+                    AssetDatabase.SaveAssets();
+                }
             }
+        }
+
+        /// <summary>
+        /// Check if the settings exist or create them.
+        /// </summary>
+        private static void CheckSettings()
+        {
+            CreateFolderIfDoesNotExists(DataFolder);
+            CreateFolderIfDoesNotExists(VersionControlFolder);
+            CreateFolderIfDoesNotExists(VersionControlEditorFolder);
+
+            settings = AssetDatabase.LoadAssetAtPath<VersionControlPackageCheckConfig>(SettingsPath);
+
+            if (settings != null) return;
+            settings = ScriptableObject.CreateInstance<VersionControlPackageCheckConfig>();
+            AssetDatabase.CreateAsset(settings, SettingsPath);
+            AssetDatabase.SaveAssets();
+        }
+
+        /// <summary>
+        /// Creates a folder if does not exist.
+        /// </summary>
+        /// <param name="path">Path to that folder.</param>
+        private static void CreateFolderIfDoesNotExists(string path)
+        {
+            if (!Directory.Exists(path)) Directory.CreateDirectory(path);
         }
     }
 }
---
+++ Editor/VersionControlLog4NetCheck/VersionControlPackageCheckConfig.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace WhateverDevs.Core.Editor.VersionControlLog4NetCheck
+{
+    /// <summary>
+    /// Scriptable object to store the preferences related to the version control package check.
+    /// </summary>
+    public class VersionControlPackageCheckConfig : ScriptableObject
+    {
+        /// <summary>
+        /// Has the user dismissed the check?
+        /// </summary>
+        public bool CheckDismissed;
+    }
+}
