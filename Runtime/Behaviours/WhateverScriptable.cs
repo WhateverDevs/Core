32ca5b5dd98ab2db36d676f4ac3cf48f1cfd8f05
Rodrigo Varga <varguiniano@gmail.com>
2022-01-29 11:49:53 +0100

    -WhateverScriptable.

---
+++ Runtime/Behaviours/WhateverScriptable.cs
@@ -0,0 +1,21 @@
+﻿using WhateverDevs.Core.Runtime.Common;
+
+namespace WhateverDevs.Core.Behaviours
+{
+    /// <summary>
+    /// Our own Scriptable Object implementation with added functionality.
+    /// </summary>
+    public class WhateverScriptable : WhateverScriptable<WhateverScriptable>
+    {
+    }
+
+    /// <summary>
+    /// Our own Scriptable Object implementation with added functionality.
+    /// It is pretty empty right now, but having it will help not having to refactor a lot in the future.
+    /// </summary>
+    /// <typeparam name="TLogger">Object to be used on the logs.</typeparam>
+    public class WhateverScriptable<TLogger> : LoggableScriptableObject<TLogger>
+        where TLogger : WhateverScriptable<TLogger>
+    {
+    }
+}
