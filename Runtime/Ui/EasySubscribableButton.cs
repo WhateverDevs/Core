0a034136924d6340c25bd9f79e6984a3e09e7688
Rodrigo Varga <varguiniano@gmail.com>
2022-02-08 16:16:42 +0100

    - Even easier to subscribe to the easy subscribable button.

--- Runtime/Ui/EasySubscribableButton.cs
+++ Runtime/Ui/EasySubscribableButton.cs
@@ -16,5 +16,29 @@ namespace WhateverDevs.Core.Runtime.Ui
         /// Call the event.
         /// </summary>
         protected override void ButtonClicked() => OnButtonClicked?.Invoke();
+
+        /// <summary>
+        /// Easy subscribing to the button.
+        /// </summary>
+        /// <param name="button">This button.</param>
+        /// <param name="subscriber">The function to subscribe.</param>
+        /// <returns>The resulting action.</returns>
+        public static EasySubscribableButton operator +(EasySubscribableButton button, Action subscriber)
+        {
+            button.OnButtonClicked += subscriber;
+            return button;
+        }
+
+        /// <summary>
+        /// Easy unsubscribing from the button.
+        /// </summary>
+        /// <param name="button">This button.</param>
+        /// <param name="subscriber">The function to unsubscribe.</param>
+        /// <returns>The resulting action.</returns>
+        public static EasySubscribableButton operator -(EasySubscribableButton button, Action subscriber)
+        {
+            button.OnButtonClicked -= subscriber;
+            return button;
+        }
     }
 }
