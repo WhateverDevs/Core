6469ffe199787b91c1bd3e01b154a1e1d3a49686
Rodrigo Varga <varguiniano@gmail.com>
2022-03-21 17:47:28 +0100

    - Descending comparer for sorted dictionaries.

---
+++ Runtime/DataStructures/DescendingComparer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WhateverDevs.Core.Runtime.DataStructures
+{
+    /// <summary>
+    /// Descending comparer to be used in SortedDictionaries.
+    /// See: https://stackoverflow.com/questions/931891/reverse-sorted-dictionary-in-net
+    /// </summary>
+    /// <typeparam name="T">Element to compare.</typeparam>
+    class DescendingComparer<T> : IComparer<T> where T : IComparable<T>
+    {
+        /// <summary>
+        /// Compare the two elements.
+        /// </summary>
+        /// <param name="x">First element.</param>
+        /// <param name="y">Second element.</param>
+        /// <returns>The order.</returns>
+        // ReSharper disable once PossibleNullReferenceException
+        public int Compare(T x, T y) => y.CompareTo(x);
+    }
+}
