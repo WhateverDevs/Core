78ca83d672afa8da42a2237465c06c539c283e77
Rodrigo Varga <varguiniano@gmail.com>
2024-09-12 13:06:01 +0200

    - Logger now only logs class name, not whole namespace.

--- Runtime/Logger/Patterns/GenericClassNamePatternLayoutConverter.cs
+++ Runtime/Logger/Patterns/GenericClassNamePatternLayoutConverter.cs
@@ -1,9 +1,11 @@
 using System.IO;
+using JetBrains.Annotations;
 using log4net.Core;
 using log4net.Layout.Pattern;
 
 namespace WhateverDevs.Core.Runtime.Logger.Patterns
 {
+    [UsedImplicitly]
     public class GenericClassNamePatternLayoutConverter : PatternLayoutConverter
     {
         protected override void Convert(TextWriter writer, LoggingEvent loggingEvent)
@@ -11,7 +13,8 @@ namespace WhateverDevs.Core.Runtime.Logger.Patterns
             string name = loggingEvent.LoggerName;
 
             name = name.Split('`')[0];
-            
+            name = name.Split('.')[^1];
+
             writer.Write(name);
         }
     }
